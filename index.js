ce68cdd3e7a2b36d50ddb3d51d87361fef344a40
Deewan Singh <deewansingh827@gmail.com>
2024-07-10 01:00:04 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { Client, GatewayIntentBits, Collection, Routes, Options } from 'discord.js';
+import { Client, GatewayIntentBits, Collection, Routes, Options,EmbedBuilder } from 'discord.js';
 import { REST } from '@discordjs/rest';
-import { Player, useQueue } from 'discord-player';
+import { Player, useMainPlayer, useQueue } from 'discord-player';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -20,44 +20,58 @@ const client = new Client({
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
     ],
-},{setTimeout:40000});
+});
 
 client.commands = new Collection();
 const player = new Player(client);
 
 
-await player.extractors.loadDefault((ext) => ext !== 'YouTubeExtractor');
+player.extractors.loadDefault((ext) => ext !== 'YouTubeExtractor');
 
 const commands = [];
 
 client.commands.set(Play.data.name, Play.execute);
-client.commands.set(Skip.data.name,Skip.execute);
-client.commands.set(Pause.data.name,Pause.execute);
-client.commands.set(Resume.data.name,Resume.execute);
-client.commands.set(Stop.data.name,Stop.execute);
+client.commands.set(Skip.data.name, Skip.execute);
+client.commands.set(Pause.data.name, Pause.execute);
+client.commands.set(Resume.data.name, Resume.execute);
+client.commands.set(Stop.data.name, Stop.execute);
 commands.push(Play.data.toJSON());
 commands.push(Skip.data.toJSON());
 commands.push(Pause.data.toJSON());
 commands.push(Resume.data.toJSON());
 commands.push(Stop.data.toJSON());
 
-client.once('ready', () => {
+client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 // console.log(player);
+player.events.on('playerError', (queue, error, track) => {
+    const embed=new EmbedBuilder();
+    embed.setDescription(track.description).setTitle('something went wrong').setThumbnail(track.thumbnail).setFooter({ text: `Duration: ${track.duration}`})
+    queue.metadata.channel.send({ embeds: [embed] });
+    // console.log(queue);
+})
+
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand() || !interaction.guildId) return;
 
+    // const player=useMainPlayer();
     if (!interaction.member.voice.channel) {
         return void interaction.reply({ content: "You are not in a voice channel!", ephemeral: true });
     }
+
+    // console.log(interaction.channelId);
 
     try {
         const func = client.commands.get(interaction.commandName);
         await func(interaction, player);
     } catch (error) {
-        console.error(error);
+        const embed = new EmbedBuilder().setTitle(`Something Went Wrong!!`)
+            .setDescription(`Something went wrong while playing `)
+            .setTimestamp(new Date());
+
+        return interaction.followUp({ embeds: [embed] });
     }
 });
 
@@ -87,5 +101,5 @@ process.on('unhandledRejection', (reason) => {
 
 // prevent crash on uncaught exception
 process.on('uncaughtException', (error) => {
-    console.error('Uncaught exception:', error);
+    console.error('Uncaught exception:', error.message);
 });
