36111fbbac6325c2d5c6f703198e4074a7838864
Deewan Singh <deewansingh827@gmail.com>
2024-08-02 03:28:36 +0530

    added youtube-extractor

--- src/clientSetup.js
+++ src/clientSetup.js
@@ -1,6 +1,7 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 import { Player } from 'discord-player';
 import dotenv from 'dotenv';
+import { YoutubeiExtractor } from "discord-player-youtubei"
 
 dotenv.config();
 
@@ -15,5 +16,12 @@ const client = new Client({
 
 const player = new Player(client);
 player.extractors.loadDefault((ext) => ext !== 'YouTubeExtractor');
+
+player.extractors.register(YoutubeiExtractor, {
+    authentication:[process.env.ACCESS_TOKEN],
+    streamOptions:{
+        useClient:'YTMUSIC'
+    }
+})
 
 export { client, player };
